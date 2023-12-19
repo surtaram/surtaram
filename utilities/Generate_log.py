4c69d86b9e2b9f81f78c08a8f4e11effe33f4aa1
jakharsurta@gmail.com <surtaram@192.168.1.15>
2023-12-20 00:24:40 +0530

    framework

--- utilities/Generate_log.py
+++ utilities/Generate_log.py
@@ -3,7 +3,7 @@ import logging
 
 def generate_logs():
     logging.basicConfig(
-        filename="Example.log",
+        filename="Automatons.logs",
         level=logging.INFO,
         format='%(asctime)s -%(levelname)s - %(message)s',
         datefmt='%Y-%m-%d-%H:%H:%S %p'
